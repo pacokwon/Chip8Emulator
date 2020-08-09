da2e8e3a7c943ae57176e8b9878b541b0ff365c8
pacokwon <haechank@gmail.com>
2020-08-09 23:44:07 +0900

    add: function implementations for emulator

--- src/emulator.c
+++ src/emulator.c
@@ -1,11 +1,36 @@
 #include "emulator.h"
 #include "cpu.h"
+#include "display.h"
+#include "input.h"
 #include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 #define MEMORY_SIZE 4096
 
-void EMU_run(struct Emulator* emu, const char* filename) {
+void EMU_run(const char* filename) {
+    struct Emulator* emu = malloc(sizeof(struct Emulator));
+    EMU_init(emu);
+    EMU_load(emu, filename);
 
+    while (true) {
+        CPU_runOperation(emu->cpu);
+
+        if (emu->cpu->drawFlag) {
+            DSP_draw(emu->display, emu->cpu->display);
+            emu->cpu->drawFlag = false;
+        }
+
+        if (emu->cpu->playSound) {
+            printf("\a");
+            emu->cpu->playSound = false;
+        }
+
+        INP_setKeys(emu->input, emu->cpu->keys);
+
+        usleep(1200 * 1000);
+    }
 }
 
 void EMU_load(struct Emulator* emu, const char* filename) {
@@ -26,4 +51,11 @@ void EMU_load(struct Emulator* emu, const char* filename) {
     CPU_loadROM(emu->cpu, (const unsigned char*)buffer);
 }
 
-void EMU_init(struct Emulator* emu) {}
+void EMU_init(struct Emulator* emu) {
+    emu->cpu = malloc(sizeof(struct CPU));
+    emu->display = malloc(sizeof(struct Display));
+    emu->input = malloc(sizeof(struct Input));
+    CPU_init(emu->cpu);
+    DSP_init(emu->display);
+    INP_init(emu->input);
+}
--- src/emulator.h
+++ src/emulator.h
@@ -1,11 +1,17 @@
 #ifndef PACO_EMU_H
 #define PACO_EMU_H
 
+#include "cpu.h"
+#include "display.h"
+#include "input.h"
+
 struct Emulator {
     struct CPU* cpu;
+    struct Display* display;
+    struct Input* input;
 };
 
-void EMU_run(struct Emulator* emu, const char* filename);
+void EMU_run(const char* filename);
 void EMU_load(struct Emulator* emu, const char* filename);
 void EMU_init(struct Emulator* emu);
 
