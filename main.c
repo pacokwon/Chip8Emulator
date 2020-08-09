bbe60bba07c5c7815036cb4b86966a43baddb78c
pacokwon <haechank@gmail.com>
2020-08-10 01:08:23 +0900

    add: main file

---
+++ main.c
@@ -0,0 +1,14 @@
+#include "src/emulator.h"
+
+int main(int argc, char** argv) {
+    if (argc >= 2) {
+        struct Emulator* emu = malloc(sizeof(struct Emulator));
+        EMU_run(emu, argv[argc - 1]);
+        EMU_close(emu);
+    } else {
+        printf("ROM file not specified!");
+        return 1;
+    }
+
+    return 0;
+}
